fd4c5d2b776552ede8ccf91f1cbb9bc67fedeb97
riorq00 <reworkk7@gmail.com>
2022-11-18 18:32:20 -0300

    chore(null-ls): enable eslint only if you have .eslintrc in root directory

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -15,12 +15,10 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettier,
-    -- null_ls.builtins.diagnostics.eslint_d.with({
-    -- diagnostics_format = '[eslint] #{m}\n(#{c})'
-    -- }),
     -- null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.eslint_d.with({ -- js/ts linter
       -- only enable eslint if root has .eslintrc.js
+      diagnostics_format = '[eslint] #{m}\n(#{c})',
       condition = function(utils)
         return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
       end,
