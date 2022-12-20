1ac64820d1c0d1f732eb49b4a76e4d99485888e1
riorq00 <reworkk7@gmail.com>
2022-12-20 18:44:10 -0300

    feat(comment): new settings

--- after/plugin/comment.rc.lua
+++ after/plugin/comment.rc.lua
@@ -1,4 +1,7 @@
-require('Comment').setup(
+local status, comment = pcall(require, "Comment")
+if (not status) then return end
+
+comment.setup(
   {
     ---Add a space b/w comment and the line
     padding = true,
@@ -8,39 +11,60 @@ require('Comment').setup(
     ignore = nil,
     ---LHS of toggle mappings in NORMAL mode
     toggler = {
-        ---Line-comment toggle keymap
-        line = 'z;',
-        ---Block-comment toggle keymap
-        block = 'zb;',
+      ---Line-comment toggle keymap
+      line = 'z;',
+      ---Block-comment toggle keymap
+      block = 'zb;',
     },
     ---LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
-        ---Line-comment keymap
-        line = 'zz;',
-        ---Block-comment keymap
-        block = 'zbb',
+      ---Line-comment keymap
+      line = 'zz;',
+      ---Block-comment keymap
+      block = 'zbb',
     },
     ---LHS of extra mappings
     extra = {
-        ---Add comment on the line above
-        above = 'z;k',
-        ---Add comment on the line below
-        below = 'z;j',
-        ---Add comment at the end of line
-        eol = 'z;l',
+      ---Add comment on the line above
+      above = 'z;k',
+      ---Add comment on the line below
+      below = 'z;j',
+      ---Add comment at the end of line
+      eol = 'z;l',
     },
     ---Enable keybindings
     ---NOTE: If given `false` then the plugin won't create any mappings
     mappings = {
-        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-        basic = true,
-        ---Extra mapping; `gco`, `gcO`, `gcA`
-        extra = true,
-        ---Extended mapping; `g>` `g<` `g>[count]{motion}` `g<[count]{motion}`
-        extended = false,
+      ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+      basic = true,
+      ---Extra mapping; `gco`, `gcO`, `gcA`
+      extra = true,
+      ---Extended mapping; `g>` `g<` `g>[count]{motion}` `g<[count]{motion}`
+      extended = false,
     },
     ---Function to call before (un)comment
-    pre_hook = nil,
+    pre_hook = function(ctx)
+      -- Only calculate commentstring for tsx filetypes
+      if vim.bo.filetype == 'typescriptreact' or 'javascriptreact' then
+        local U = require('Comment.utils')
+
+        -- Determine whether to use linewise or blockwise commentstring
+        local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
+
+        -- Determine the location where to calculate commentstring from
+        local location = nil
+        if ctx.ctype == U.ctype.blockwise then
+          location = require('ts_context_commentstring.utils').get_cursor_location()
+        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+          location = require('ts_context_commentstring.utils').get_visual_start_location()
+        end
+
+        return require('ts_context_commentstring.internal').calculate_commentstring({
+          key = type,
+          location = location,
+        })
+      end
+    end,
     ---Function to call after (un)comment
     post_hook = nil,
   }
